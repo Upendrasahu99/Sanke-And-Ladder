8652e1348b14121991de0c3ae952d28de3f97d83
upendra <upendrasahu1199@gmail.com>
2023-02-06 11:23:38 +0530

    Plyer 1 Starting Position

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Snake and Ladder");
+        int position=0;
+        int playerCount=1;
+        int playerNumber=1;
+        System.out.println("Welcome Player " + playerNumber);
+    }
+}
